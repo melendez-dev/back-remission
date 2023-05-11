de6261ffb30e09d7cacda39cbf1089e0f5f2a766
melendez-dev <sams20030818@gmail.com>
2023-05-11 12:53:52 -0500

    debug 13

--- src/router/login.js
+++ src/router/login.js
@@ -21,7 +21,6 @@ loginRouter.post("/", async (req, res) => {
   try {
     const { body } = req;
     if (!body?.username || !body?.password) {
-      utils.errorReponse(res, 204, "Correo y/o contraseña requeridas");
       return;
     }
     const query = `SELECT id, username, rol FROM access WHERE username="${body.username}" and password="${body.password}"`;
