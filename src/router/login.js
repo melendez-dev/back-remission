34ddc6933c26593008cc91637fa0e231d2cca671
melendez-dev <sams20030818@gmail.com>
2023-05-11 12:09:09 -0500

    debug 08

--- src/router/login.js
+++ src/router/login.js
@@ -13,17 +13,18 @@ loginRouter.get("/", (req, res) => {
 loginRouter.post("/", async (req, res) => {
   try {
     const { body } = req;
-    if (!body.username || !body.password) {
-      utils.errorReponse(res, 204, "Correo y/o contraseña requeridas");
-      return;
-    }
-    const query = `SELECT id, username, rol FROM access WHERE username="${body.username}" and password="${body.password}"`;
-    //const data = await db.handleQuery(query);
-    utils.sucessResponse(
-      res,
-      [],
-      data?.length ? "success" : "no existe el usuario"
-    );
+    res.send("ok")
+    // if (!body.username || !body.password) {
+    //   utils.errorReponse(res, 204, "Correo y/o contraseña requeridas");
+    //   return;
+    // }
+    // const query = `SELECT id, username, rol FROM access WHERE username="${body.username}" and password="${body.password}"`;
+    // const data = await db.handleQuery(query);
+    // utils.sucessResponse(
+    //   res,
+    //   [],
+    //   data?.length ? "success" : "no existe el usuario"
+    // );
   } catch (e) {
     utils.errorReponse(res, 500, e);
   }
