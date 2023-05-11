3d71d5f2105a5bcdd0b7b3ad4ee9a9175f98e0a3
melendez-dev <sams20030818@gmail.com>
2023-05-11 11:41:56 -0500

    debug 01

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const PORT = 3001;
 
 // import routers
 const loginRouter = require('./src/router/login.js');
-const uploadRouter = require('./src/router/upload.js');
-const usersRouter = require('./src/router/users.js');
-const productsRouter = require('./src/router/products.js');
-const remissionRouter = require('./src/router/remission.js');
-const pdfRouter = require('./src/router/pdf.js');
-const boxRouter = require('./src/router/box.js');
-const boxMovementRouter = require('./src/router/box_movement.js');
+// const uploadRouter = require('./src/router/upload.js');
+// const usersRouter = require('./src/router/users.js');
+// const productsRouter = require('./src/router/products.js');
+// const remissionRouter = require('./src/router/remission.js');
+// const pdfRouter = require('./src/router/pdf.js');
+// const boxRouter = require('./src/router/box.js');
+// const boxMovementRouter = require('./src/router/box_movement.js');
 
 // import middleware
-const middlewareHeaders = require('./src/middlewares/headers.js');
+//const middlewareHeaders = require('./src/middlewares/headers.js');
 
 // create app
 const app = express();
@@ -28,13 +28,13 @@ app.use(cors({
     origin: "*"
 }));
 
-app.use((req, res, next) => middlewareHeaders(req, res, next)) // validate headers
 app.use(express.static('public')); // static image
 app.set('view engine', 'hbs'); // allow hbs files
 
 
 // http request
-// app.use('/login', loginRouter);
+//app.use((req, res, next) => middlewareHeaders(req, res, next)) // validate headers
+app.use('/login', loginRouter);
 // app.use('/upload', uploadRouter);
 // app.use('/users', usersRouter);
 // app.use('/products', productsRouter)
